2961b82b4ae247ba77eaeafb1a5953b01a0132ed
ubaig517 <ubaig517@gmail.com>
2023-08-22 02:21:41 -0400

    Write endpoints to update and delete prompts. Create user profile page and write functionality.

---
+++ app/profile/page.jsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import Profile from "@components/Profile";
+
+const ProfilePage = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+
+      setPosts(data);
+    };
+
+    if (session?.user.id) fetchPosts();
+  }, []);
+
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
+
+  const handleDelete = async (post) => {};
+
+  return (
+    <Profile
+      name="My"
+      desc="Welcome to your profile page"
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
+};
+
+export default ProfilePage;
